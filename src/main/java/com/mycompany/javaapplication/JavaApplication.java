2b9cb40a595d43218034b3ff9a98c91ea8ecf764
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-03-17 22:14:48 -0300

    Added a Christmas tree logic

--- src/main/java/com/mycompany/javaapplication/JavaApplication.java
+++ src/main/java/com/mycompany/javaapplication/JavaApplication.java
@@ -11,6 +11,32 @@ package com.mycompany.javaapplication;
 public class JavaApplication {
 
     public static void main(String[] args) {        
-        System.out.println("Hello World!");
+        int height = 10;
+        
+        for (int row = 0; row < height; row++) {
+            for (int space = 0; space < height-row-1; space++) {
+                System.out.print(" ");
+            }
+            
+            for (int asterisk = 0; asterisk < row*2+1; asterisk++) {
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }        
+        
+        int trunkLength = 3;
+        
+        for (int base = 0; base < trunkLength; base++) {
+            for (int space = 0; space < height-2; space++) {
+                System.out.print(" ");
+            }
+            
+            for (int trunk = 0; trunk < 3; trunk++) {
+                System.out.print("|");
+            }
+            
+            System.out.println("");
+        }
     }
 }
